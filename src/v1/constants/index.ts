818b7f71fc7e8f7eda9cb0b4d5b70a74d74ed8e9
cometburn <cometburn@gmail.com>
2026-02-10 15:58:15 +0800

    Add room status

--- src/v1/constants/index.ts
+++ src/v1/constants/index.ts
@@ -3,4 +3,6 @@ export const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET as string;
 
 export const ACCESS_TOKEN_EXPIRATION = "1s";
 export const REFRESH_TOKEN_EXPIRATION = "7d";
-export const COOKIE_MAX_AGE = 7 * 24 * 60 * 60 * 1000; // 7 days
+export const COOKIE_MAX_AGE = 7 * 24 * 60 * 60 * 1000;
+
+export const ROOM_STATUS = ["available", "occupied", "maintenance"];
