8fe2e6d6a4eddb49ccab54aef7ae7eb2156bbf73
cometburn <cometburn@gmail.com>
2026-02-16 20:23:28 +0800

    Add constants

--- src/v1/constants/index.ts
+++ src/v1/constants/index.ts
@@ -1,9 +1,14 @@
 export const JWT_SECRET = process.env.JWT_SECRET as string;
 export const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET as string;
 
+export const TIMEZONE = "Asia/Manila";
+
 export const ACCESS_TOKEN_EXPIRATION = "1s";
 export const REFRESH_TOKEN_EXPIRATION = "7d";
 export const COOKIE_MAX_AGE = 7 * 24 * 60 * 60 * 1000;
 
 export const ROOM_STATUS = ["available", "occupied", "maintenance"];
 export const OPERATIONAL_STATUS = ["available", "maintenance", "out of service"];
+export const BOOKING_STATUS = ["check_in", "check_out", "reserved", "transfered", "cancelled", "no_show"];
+export const PAYMENT_STATUS = ["unpaid", "paid", "refunded"];
+export const PAYMENT_TYPE = ["cash", "credit_card", "debit_card", "bank", "e-wallet"]
