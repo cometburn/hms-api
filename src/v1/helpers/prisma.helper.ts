3f754cdfdc6fa3172b9edccfebec68139c853dc4
cometburn <cometburn@gmail.com>
2026-02-28 10:09:50 +0800

    Edit helper

--- src/v1/helpers/prisma.helper.ts
+++ src/v1/helpers/prisma.helper.ts
@@ -3,9 +3,9 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient({
     datasources: {
         db: {
-            url: process.env.DATABASE_URL
-        }
-    }
+            url: process.env.DATABASE_URL,
+        },
+    },
 });
 
 export default prisma;
