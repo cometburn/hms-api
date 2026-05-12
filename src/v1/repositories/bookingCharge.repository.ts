80d39d569eac00ed789ba7c723177440c292893f
cometburn <cometburn@gmail.com>
2026-05-12 21:29:23 +0800

    Add create charge repo

--- src/v1/repositories/bookingCharge.repository.ts
+++ src/v1/repositories/bookingCharge.repository.ts
@@ -9,3 +9,12 @@ import { BookingCharge } from "@prisma/client";
 export const createManyCharges = async (data: Array<BookingCharge>) => {
     return await prisma.bookingCharge.createMany({ data });
 }
+
+/**
+ * Create Booking Charge
+ * @param data 
+ * @returns 
+ */
+export const createCharge = async (data: Omit<BookingCharge, "id" | "created_at" | "updated_at">) => {
+    return await prisma.bookingCharge.create({ data });
+}   
